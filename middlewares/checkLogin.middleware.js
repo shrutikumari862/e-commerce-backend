ffef151b8cb1572e43836cda5cb40ec9e34772df
shrutikumari862 <810kumarishruti@gmail.com>
2025-08-27 00:08:51 +0530

    update

--- middlewares/checkLogin.middleware.js
+++ middlewares/checkLogin.middleware.js
@@ -5,7 +5,7 @@ dotenv.config()
 export const chekLogin=async(req,res,next)=>{
        try {
            const verifyUser=await jwt.verify(req.headers.authorization.split(' ')[1],process.env.JWT_SECRET)
-           req.user = decoded;
+           
            next()
        } catch (error) {
         return res.status(500).json({
